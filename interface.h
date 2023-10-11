0dc38f9a0291aa034161dd8b0226d20b57fbfb67
kierstinhavens <92461009+kierstinhavens@users.noreply.github.com>
2023-10-10 19:20:16 -0700

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,23 +1,25 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-#include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
+#include "lexanalyzer.h"
+#include "expevaluator.h"
 
 using namespace std;
 
 class Interface
 {
 	friend class lexAnalyzer;
+	friend class expEvaluator;
 
 public:
 	void startInterface();
-	void input(Interface&, lexAnalyzer&);
-	void helpCommand(Interface&, lexAnalyzer&);
-	void showCommand(Interface&, lexAnalyzer&);
-	void readCommand(Interface&, lexAnalyzer&);
-	void clearCommand(Interface&, lexAnalyzer&);
+	void input(Interface&, lexAnalyzer&, expEvaluator&);
+	void helpCommand(Interface&, lexAnalyzer&, expEvaluator&);
+	void showCommand(Interface&, lexAnalyzer&, expEvaluator&);
+	void readCommand(Interface&, lexAnalyzer&, expEvaluator&);
+	void clearCommand(Interface&, lexAnalyzer&, expEvaluator&);
 	Interface();
 private:
 	typedef vector<string> programType;
