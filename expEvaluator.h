638ed515f5fbb65fc4c631846885ba3cb74a4eea
Kierstin Havens <92461009+kierstinhavens@users.noreply.github.com>
2023-11-15 20:39:31 -0800

    Update expEvaluator.h

--- expEvaluator.h
+++ expEvaluator.h
@@ -6,25 +6,39 @@
 #include <utility>
 #include <stack>
 #include "lexanalyzer.h"
+#include "interpreter.h"
+#include "interface.h"
 
 using namespace std;
+
+enum class categoryType;
 
 class expEvaluator
 {
 
 	friend class Interface;
 	friend class lexAnalyzer;
+	friend class Interpreter;
 
 public:
 
 	expEvaluator();
-	void startExp(Interface&, lexAnalyzer&, expEvaluator&);
-	void infixToPostfix(Interface&, lexAnalyzer&, expEvaluator&);
+	void startExp(Interface&, lexAnalyzer&, expEvaluator&, Interpreter&);
+	void infixToPostfix(Interface&, lexAnalyzer&, expEvaluator&, Interpreter&);
+	void postfixEval(Interface&, lexAnalyzer&, expEvaluator&, Interpreter&);
+	int getPrecedence(string);
+	int calculate(int, int, string);
+	void storeTable(string, string);
+	int getTableVal(string, Interface&, lexAnalyzer&, expEvaluator&, Interpreter&);
+	void clearTable();
+	void showVariables(Interface&, lexAnalyzer&, expEvaluator&, Interpreter&);
 
 private:
 
-	vector<pair<string, categoryType>> postVect;
+	vector<pair<string, categoryType>> postVect, inVect, stackVect;
 	typedef map<string, string> symbolTableType;
+	typedef pair<string, categoryType> pairType;
 	symbolTableType symbolTable;
 };
 #endif
+
